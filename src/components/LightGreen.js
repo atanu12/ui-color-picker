978692d2248aab2a78f5cade6defa4f15e924001
Atanu Mondal <atanumandal45@gmail.com>
2021-02-16 17:28:44 +0530

    added the lightGreen Color

---
+++ src/components/LightGreen.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+
+const LightGreen = () => {
+  const LightGreenColor = [
+    "#dcedc8",
+    "#c5e1a5",
+    "#aed581",
+    "#9ccc65",
+    "#8bc34a",
+    "#7cb342",
+    "#689f38",
+    "#558b2f",
+    "#33691e",
+  ];
+
+  const [background, setBackground] = useState("#071415");
+  const [current, setCurrent] = useState(null);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setCurrent(null);
+    }, 1000);
+    return () => clearTimeout(timeout);
+  }, [current]);
+  return (
+    <div>
+      <div className="title">
+        <h1>LightGreen Color</h1>
+      </div>
+      <div className="container">
+        {LightGreenColor.map((color) => (
+          <CopyToClipboard text={`${color}`}>
+            <div key={color.id} className="card">
+              <div
+                style={{
+                  background: color,
+                }}
+                className="box"
+              ></div>
+              <p
+                style={{
+                  color: "#fff",
+                  textTransform: "uppercase",
+                }}
+                onClick={() => setCurrent(color)}
+                className="para"
+              >
+                {color}
+              </p>
+            </div>
+          </CopyToClipboard>
+        ))}
+      </div> 
+    </div>
+  );
+};
+
+export default LightGreen;
